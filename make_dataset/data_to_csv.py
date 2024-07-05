dc65e464f347e6e28a0348bcf3712b42301e143f
holyxieter <119303613+holyxieter@users.noreply.github.com>
2024-07-05 16:43:10 +0800

    Update data_to_csv.py

--- make_dataset/data_to_csv.py
+++ make_dataset/data_to_csv.py
@@ -5,7 +5,7 @@ start_time = '2024-01-01 00:00:00'
 end_time = '2024-06-30 23:55:00'
 time_points = pd.date_range(start=start_time, end=end_time, freq='5min')
 
-scol = pd.read_csv('../speed_data.csv')
+scol = pd.read_csv('../speed_data.csv') # 使用旧文件获取所有传感器id
 sensor = scol.keys().tolist()[1:]
 
 data = np.load('PEMSBAY_2024.npy')
