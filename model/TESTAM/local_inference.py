581c1ed51349feef7b4764fc727a715a3b5d66f1
holyxieter <119303613+holyxieter@users.noreply.github.com>
2024-07-10 23:19:48 +0800

    Update local_inference.py

--- model/TESTAM/local_inference.py
+++ model/TESTAM/local_inference.py
@@ -151,8 +151,8 @@ def predict():
     result = inference(hms, pretime)
     result_json = result.to_json(orient="records")
 
-    return result_json
+    return jsonify(result_json)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='127.0.0.1', port=5000)
