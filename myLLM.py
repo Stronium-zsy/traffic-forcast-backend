dc2dac71c79a51323650f17f6ba0bc364350b82e
holyxieter <119303613+holyxieter@users.noreply.github.com>
2024-07-11 15:54:46 +0800

    Add files via upload

---
+++ myLLM.py
@@ -0,0 +1,19 @@
+from http import HTTPStatus
+from dashscope import Application
+
+
+def call_agent_app(prompt):
+    response = Application.call(app_id='0d67e26286e54f9b9f20d52175493733',
+                                prompt=prompt,
+                                api_key='sk-08431a08d23b4ed6a9942277c6c182e3')
+
+    if response.status_code != HTTPStatus.OK:
+        print('request_id=%s, code=%s, message=%s\n' % (response.request_id, response.status_code, response.message))
+    else:
+        print(response.output.text)
+
+
+if __name__ == '__main__':
+    # 2024年3月4日晚上18点，我想从Ames Research Center 前往 San Jose的火车站，如何规划路线？
+    user_prompt = input("请输入提示词>\n")
+    call_agent_app(user_prompt)
